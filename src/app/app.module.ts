d5b831489825eb23f07cd69a285fd73b7fa812f3
unzaza22 <suwunorder@gmail.com>
2025-05-24 13:11:45 +0700

    Update mean-course
    Section 2: The Angular Frontend - Understanding the Basics
    27. Getting Posts from Post-Create to Post-List
    28. Calling GET Post
    29. More About Observables
    30. Working on our Form
    31. Section Resources
    Section 3: Adding NodeJS to our Project
    32. Module Introduction
    33. Connecting Node & Angular - Theory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HeaderComponent } from './header/header.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component'
 import { PostListComponent } from './posts/post-list/post-list.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
